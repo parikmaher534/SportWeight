9f5bcdb140bf25bf9c2100fe689665846a6b8a9d
Nikolay Podstrigan <parikmaher534@gmail.com>
2015-03-10 00:40:50 +0300

    Generate Item api

---
+++ Sport/api/controllers/ItemsController.js
@@ -0,0 +1,11 @@
+/**
+ * ItemsController
+ *
+ * @description :: Server-side logic for managing items
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ Sport/api/models/Items.js
@@ -0,0 +1,44 @@
+/**
+* Items.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+    attributes: {
+
+        name: {
+            type: 'string',
+            required: true
+        },
+
+        category: {
+            type: 'string',
+            required: true
+        },
+
+        description: {
+            type: 'string'
+        },
+
+        price: {
+            type: 'string'
+        },
+
+        photo: {
+            type: 'string'
+        },
+
+        photoFull: {
+            type: 'string'
+        },
+
+        manufacturer: {
+            type: 'string'
+        }
+    }
+
+};
+
